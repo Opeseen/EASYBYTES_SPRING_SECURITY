fd5a992c7d2d38ed7f9313f8f94c9bd036999ee0
Opeyemi <Opeyemi5055@yahoo.com>
2025-03-15 18:36:21 +0100

    added some section lecture

--- springsecsection7/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ springsecsection7/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.easybytes.config;
 
+import com.easybytes.exceptionHandling.CustomAccessDeniedHandler;
+import com.easybytes.exceptionHandling.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -21,19 +23,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ProjectSecurityConfig {
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf( csrfConfig -> csrfConfig.disable())
+    http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession")
+            .maximumSessions(3))
+        .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // Only HTTP
+        .csrf( csrfConfig -> csrfConfig.disable())
         .authorizeHttpRequests((requests) -> requests
           .requestMatchers("/myAccount","/myBalance","myLoans").authenticated()
-          .requestMatchers("notices","/contact","/error", "/register").permitAll()
+          .requestMatchers("notices","/contact","/error", "/register", "invalidSession").permitAll()
     );
     http.formLogin(withDefaults());
-    http.httpBasic(withDefaults());
+    http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+    http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
     return http.build();
   }
-//  @Bean
-//  public UserDetailsService userDetailsService(DataSource dataSource){
-//    return new JdbcUserDetailsManager(dataSource);
-//  }
   @Bean
   public PasswordEncoder passwordEncoder(){
     return PasswordEncoderFactories.createDelegatingPasswordEncoder();
--- springsecsection7/src/main/java/com/easybytes/config/ProjectSecurityProdConfig.java
+++ springsecsection7/src/main/java/com/easybytes/config/ProjectSecurityProdConfig.java
@@ -1,39 +1,36 @@
 package com.easybytes.config;
 
+import com.easybytes.exceptionHandling.CustomAccessDeniedHandler;
+import com.easybytes.exceptionHandling.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@Profile("!prod")
-public class ProjectSecurityConfig {
+@Profile("prod")
+public class ProjectSecurityProdConfig {
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf( csrfConfig -> csrfConfig.disable())
+    http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession"))
+        .requiresChannel(rcc -> rcc.anyRequest().requiresSecure()) // Only HTTPS
+        .csrf( csrfConfig -> csrfConfig.disable())
         .authorizeHttpRequests((requests) -> requests
           .requestMatchers("/myAccount","/myBalance","myLoans").authenticated()
-          .requestMatchers("notices","/contact","/error", "/register").permitAll()
+          .requestMatchers("notices","/contact","/error", "/register", "invalidSession").permitAll()
     );
     http.formLogin(withDefaults());
-    http.httpBasic(withDefaults());
+    http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+    http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
     return http.build();
   }
-//  @Bean
-//  public UserDetailsService userDetailsService(DataSource dataSource){
-//    return new JdbcUserDetailsManager(dataSource);
-//  }
   @Bean
   public PasswordEncoder passwordEncoder(){
     return PasswordEncoderFactories.createDelegatingPasswordEncoder();
