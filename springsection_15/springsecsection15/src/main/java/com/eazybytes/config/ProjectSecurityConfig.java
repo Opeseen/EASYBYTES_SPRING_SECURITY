ef88048711ee76b9202b40827a6d78a9b63647a7
Opeyemi <Opeyemi5055@yahoo.com>
2025-11-05 14:51:44 +0100

    keycloak cntd

---
+++ springsection_15/springsecsection15/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,78 @@
+package com.eazybytes.config;
+
+import com.eazybytes.exceptionHandling.CustomAccessDeniedHandler;
+import com.eazybytes.exceptionHandling.CustomBasicAuthenticationEntryPoint;
+import com.eazybytes.filter.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("!prod")
+public class ProjectSecurityConfig {
+
+    // for opaque token application.properties injection
+    /*
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-uri}")
+    String introspectionUri;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-id}")
+    String clientId;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-secret}")
+    String clientSecret;
+     */
+
+  @Bean
+  SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+    CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+    http
+        .sessionManagement(sessionConfig ->
+            sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //create a session to be used to access resources
+        .cors(cors -> cors.configurationSource(new CorsConfigFilter()))
+        .csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+            .ignoringRequestMatchers( "/contact","/register")
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+        .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // Only HTTP
+        .authorizeHttpRequests((requests) -> requests
+          .requestMatchers("/myAccount").hasRole("USER")
+          .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+          .requestMatchers("/myLoans").authenticated()
+          .requestMatchers("/myCards").hasRole("USER")
+          .requestMatchers("/user").authenticated()
+          .requestMatchers("notices","/contact","/error", "/register").permitAll()
+    );
+    // jwt token example
+    http.oauth2ResourceServer(rsc -> rsc.jwt(jwtConfigurer
+        -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+    // Opaque token example
+    /*http.oauth2ResourceServer(rsc ->
+        rsc.opaqueToken(atc ->
+            atc.authenticationConverter(new KeycloakOpaqueRoleConverter())
+                .introspectionUri(this.introspectionUri)
+                .introspectionClientCredentials(this.clientId, this.clientSecret)));*/
+    http.exceptionHandling(ehc ->
+        ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
+    return http.build();
+  }
+}
